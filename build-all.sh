9745d65a3f2974578c91f347e98d521f566973f2
primbs <primbs@fim.uni-passau.de>
2023-12-08 16:36:01 +0000

    added logs

--- build-all.sh
+++ build-all.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+readonly GREEN='\033[0;32m'
+readonly NOCOLOUR='\033[0m'
+function echo_colour() {
+	local -r colour="$1"
+	local -r message="$2"
+
+	echo -e "${colour}${message}${NOCOLOUR}"
+}
 
 bash gradlew clean runnableJar
 
@@ -31,6 +39,7 @@ for model in "${MODEL_ARRAY[@]}"
 do
   for assertion_number in "${ASSERTION_ARRAY[@]}"
   do
+    echo_colour "$GREEN" "${model} with ${assertion_number} number of assertions"
     ${JAVA} -jar ${ASSERTION_JAR}  --data-dir "${BASE_FILE}" --save-dir "${SAVE_DIR}/${assertion_number}" --model "${model}" -m "${assertion_number}"
   done
 done
