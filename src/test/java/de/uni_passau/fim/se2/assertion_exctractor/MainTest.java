850c05ae33360df46142606cc484e9768f62b809
sprimbs <53371440+sprimbs@users.noreply.github.com>
2024-03-20 10:33:00 +0100

    added main test

--- src/test/java/de/uni_passau/fim/se2/assertion_exctractor/MainTest.java
+++ src/test/java/de/uni_passau/fim/se2/assertion_exctractor/MainTest.java
@@ -1,26 +1,38 @@
 package de.uni_passau.fim.se2.assertion_exctractor;
 
+import ch.qos.logback.core.util.FileUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.uni_passau.fim.se2.assertion_exctractor.data.RawMethodData;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.junit.rules.TemporaryFolder;
+import picocli.CommandLine;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.contentOf;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     @TempDir
     Path tempDir;
     @Test
-    void testMainRun() throws IOException {
+    void testMainRun() throws URISyntaxException, IOException {
         URL r = Thread.currentThread().getContextClassLoader().getResource("example_files/results-small.jsonl");
-        assert r != null;
-        Main.main(new String[] {"preprocess","--data-dir",r.getPath(),"--save-dir",tempDir.toString(),"--model","atlas:toga:code2seq:asserT5:gpt", "-m","1"});
+        assertThat(r).isNotNull();
+        Path copiedFile = Path.of(tempDir.toString(), "result.jsonl");
+        Files.copy(Paths.get(r.toURI()), copiedFile);
+        String[] args = new String[] {"preprocess","--data-dir", copiedFile.toString(),"--save-dir",tempDir.toString(),"--model","atlas:toga:code2seq:asserT5:gpt", "-m","1"};
+        new CommandLine(new Main()).execute(args);
     }
+
 
 }
